32d969768efeba241d34ad9b0ad3323da9d2c7ec
Svitlana Shumal <svetlankachajka@gmail.com>
2025-07-17 21:55:47 +0300

    added files and tasks blended

---
+++ src/task-12.ts
@@ -0,0 +1,11 @@
+// Задача 12
+
+// Є функція sendDoneStatus:
+
+// function sendDoneStatus(callback) {
+//   callback("done");
+// }
+
+// Завдання:
+
+// Типізуйте параметр callback, щоб це була функція, яка приймає рядок і повертав void.
